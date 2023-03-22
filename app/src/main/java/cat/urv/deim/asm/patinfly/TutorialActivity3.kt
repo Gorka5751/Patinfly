8f047a71862a4749a83a961855acaa576d8bc0c8
Gorka Zamorano <gorka5751@gmail.com>
2023-03-22 21:10:48 +0100

    Hem creat el tutorial

---
+++ app/src/main/java/cat/urv/deim/asm/patinfly/TutorialActivity3.kt
@@ -0,0 +1,10 @@
+package cat.urv.deim.asm.patinfly
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TutorialActivity3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
